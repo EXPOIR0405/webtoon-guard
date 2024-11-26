054acb3d54b336f0ce3befc2dd28568f68c61f74
minjung <rkdalswjd0405@gmail.com>
2024-11-26 23:56:00 +0900

    안쓰는 것들 삭제

--- src/app/copyright-guide/page.js
+++
@@ -1,9 +0,0 @@
-import { CopyrightGuide as CopyrightGuideComponent } from '@/components/CopyrightGuide';
-
-export default function Page() {
-  return (
-    <div>
-      <CopyrightGuideComponent />
-    </div>
-  );
-}
