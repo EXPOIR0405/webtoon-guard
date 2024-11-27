f42291991d8076a910d8514812838551494f5d36
minjung <rkdalswjd0405@gmail.com>
2024-11-27 12:40:27 +0900

    잘리는 것 수정

--- src/components/HallOfShame/index.js
+++ src/components/HallOfShame/index.js
@@ -5,7 +5,7 @@ export default function HallOfShame() {
   return (
     <div className="min-h-screen bg-gray-50 py-12">
       <div className="container mx-auto px-4">
-        <h1 className="text-3xl font-bold text-center mb-12 text-black">
+        <h1 className="text-3xl mt-12 font-bold text-center mb-12 text-black">
           불법 웹툰 운영자 명예의 전당
         </h1>
         
