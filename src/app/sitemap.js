d4e8268f2e4eb3cc37024e944cd75cafd1834af3
minjung <rkdalswjd0405@gmail.com>
2024-11-28 18:06:12 +0900

    사이트맵 태그 수정

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,26 +1,22 @@
-export default function sitemap() {
-    const baseUrl = 'https://www.grimkeeper.co.kr'
-    
-    const routes = [
-      {
-        url: baseUrl,
-        lastModified: new Date().toISOString(),
-        changeFrequency: 'weekly',
-        priority: 1,
-      },
-      {
-        url: `${baseUrl}/legal`,
-        lastModified: new Date().toISOString(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-      {
-        url: `${baseUrl}/legalsupport`,
-        lastModified: new Date().toISOString(),
-        changeFrequency: 'monthly',
-        priority: 0.8,
-      },
-    ]
-  
-    return routes
-  }
+export default async function sitemap() {
+  return [
+    {
+      url: 'https://www.grimkeeper.co.kr',
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'weekly',
+      priority: 1,
+    },
+    {
+      url: 'https://www.grimkeeper.co.kr/legal',
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+    {
+      url: 'https://www.grimkeeper.co.kr/legalsupport',
+      lastModified: new Date().toISOString(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
+  ]
+}
